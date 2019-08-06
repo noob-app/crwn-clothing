de0815442d4c35cdf49825913398f6e6cc5d7dbd
Noob App <noob-app@Saileshs-MacBook-Pro.local>
2019-08-06 22:55:04 +0545

    add header component and link to home, shop and contact

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => <div>Hats Page</div>;
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div className="container">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
