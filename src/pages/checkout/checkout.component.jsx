9bf6a2f7c808ecbdebf926f73aa399035ca04a31
Noob App <noob-app@Saileshs-MacBook-Pro.local>
2019-08-12 23:32:17 +0545

    moved our directory and shop data into its own reducer, created respective flow for directory/shop

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -31,7 +31,7 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">
-      <spam>TOTAL: ${total}</spam>
+      <span>TOTAL: ${total}</span>
     </div>
   </div>
 );
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import CollectionsOverviewComponent from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = () => (
+const ShopPage = ({ match }) => (
   <div className="shop-page">
     <Switch>
-      <Route component={CollectionsOverviewComponent} />
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverviewComponent}
+      />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </Switch>
   </div>
 );
